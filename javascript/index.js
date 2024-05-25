5883336ba46be3ba61437f8e8c4802d8ecde8936
Muhammad Mudassir <mudassirkhn940@gmail.com>
2024-05-25 20:33:07 +0530

    first change

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+ScrollReveal({ 
+    reset: true,
+    distance :"60px",
+    duration :2500,
+    delay :400 
+ });
+    ScrollReveal().reveal('h1,.banner2_img,.banner3_heading h2', { delay: 200 , origin:'top'});
+
+    ScrollReveal().reveal('.banner4_sec', { opacity:0, scale:0.8 ,interval: 600   });
+   ScrollReveal().reveal('.banner2_sec,.banner4_img', {opacity:0, scale:0.8 ,interval: 600    });
